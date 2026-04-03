df6ec85adfc713dabca37cc1f724e79349ed41e8
Daniil669 <daniil.freeman669@gmail.com>
2026-04-03 12:52:46 +0300

    fixed auth test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,9 +1,7 @@
 package auth
 
 import (
-	"errors"
 	"net/http"
-	"reflect"
 	"testing"
 )
 
@@ -11,20 +9,26 @@ func TestAuth(t *testing.T) {
 	type test struct {
 		input       http.Header
 		output      string
-		outputError error
+		outputError string
 	}
 
 	tests := []test{
-		{input: http.Header{"Authorization": []string{"ApiKey my-secret-key"}}, output: "my-secret-key", outputError: nil},
-		{input: http.Header{}, output: "", outputError: errors.New("no authorization header included")},
-		{input: http.Header{"Authorization": []string{"ApiKey "}}, output: "", outputError: errors.New("malformed authorization header")},
-		{input: http.Header{"Authorization": []string{"APIKEY my-secret-key "}}, output: "", outputError: errors.New("malformed authorization header")},
+		{input: http.Header{"Authorization": []string{"ApiKey my-secret-key"}}, output: "my-secret-key", outputError: ""},
+		{input: http.Header{}, output: "", outputError: "no authorization header included"},
+		{input: http.Header{"Authorization": []string{"ApiKey "}}, output: "", outputError: "malformed authorization header"},
+		{input: http.Header{"Authorization": []string{"APIKEY my-secret-key "}}, output: "", outputError: "malformed authorization header"},
 	}
 
 	for i, tc := range tests {
 		gotOutput, gotError := GetAPIKey(tc.input)
-		if !reflect.DeepEqual(tc.output, gotOutput) {
-			t.Fatalf("test %d: expected: %v, gotOutput: %v, gotError: %v", i+1, tc.output, gotOutput, gotError)
+		if gotError != nil {
+			if gotError.Error() != tc.outputError {
+				t.Errorf("test %d: expected Error: %v, got Error: %v", i+1, tc.outputError, gotError)
+				return
+			}
+		}
+		if gotOutput != tc.output {
+			t.Errorf("test %d: expected: %v, gotOutput: %v", i+1, tc.output, gotOutput)
 		}
 	}
 }
